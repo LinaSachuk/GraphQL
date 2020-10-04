2de2d7ae9e66124f63bd01091c070e5788964ac1
Lina Sachuk <sachuklina@gmail.com>
2020-10-04 00:08:07 -0600

    add graphql server

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import graphqlHTTP from 'express-graphql';
+// import graphqlHTTP from 'express-graphql';
 import schema from './schema';
 
 const app = express();
@@ -8,12 +8,18 @@ app.get('/', (req, res) => {
     res.send('GraphQL is amazing!');
 });
 
+
 const root = { hello: () => "Hi, I'm Manny" };
+const { graphqlHTTP } = require('express-graphql');
+
+
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
     rootValue: root,
     graphiql: true,
 }))
+
+
 
 app.listen(8080, () => console.log('Running server on port localhost:8080/graphql'));
