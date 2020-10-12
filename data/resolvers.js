59130396269f133b70f059bd021e6b9b26b001f5
Lina Sachuk <sachuklina@gmail.com>
2020-10-12 00:56:08 -0600

    add alien db

--- data/resolvers.js
+++ data/resolvers.js
@@ -1,23 +1,31 @@
 import mongoose from 'mongoose';
-import { Friends } from './dbConnectors';
+import { Friends, Aliens } from './dbConnectors';
 
-// resolver map 
+// resolver map
 export const resolvers = {
     Query: {
-        getFriend: ({ id }) => {
-            return new Friend(id, friendDatabase[id])
+        getOneFriend: (root, { id }) => {
+            return new Promise((resolve, object) => {
+                Friends.findById(id, (err, friend) => {
+                    if (err) reject(err)
+                    else resolve(friend)
+                });
+            });
         },
+        getAliens: () => {
+            return Aliens.findAll();
+        }
     },
     Mutation: {
         createFriend: (root, { input }) => {
-            const newFriend = new Friend({
+            const newFriend = new Friends({
                 firstName: input.firstName,
                 lastName: input.lastName,
                 gender: input.gender,
                 age: input.age,
                 language: input.language,
                 email: input.email,
-                contacts: input.contacts,
+                contacts: input.contacts
             });
 
             newFriend.id = newFriend._id;
@@ -28,10 +36,22 @@ export const resolvers = {
                     else resolve(newFriend)
                 })
             })
-
         },
+        updateFriend: (root, { input }) => {
+            return new Promise((resolve, object) => {
+                Friends.findOneAndUpdate({ _id: input.id }, input, { new: true }, (err, friend) => {
+                    if (err) reject(err)
+                    else resolve(friend)
+                })
+            })
+        },
+        deleteFriend: (root, { id }) => {
+            return new Promise((resolve, object) => {
+                Friends.remove({ _id: id }, (err) => {
+                    if (err) reject(err)
+                    else resolve('Successfully deleted friend')
+                })
+            })
+        }
     },
 };
-
-// export resolvers
-export default resolvers;
